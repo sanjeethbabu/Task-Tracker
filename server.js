79065efd9920200cdc7529e174fba8a52dfc43b0
Sanjeeth Babu <sanjeeth.babu@gantecusa.com>
2026-06-08 23:09:47 +0530

    changed the todo and fixed the server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
  const express = require('express');
 const cors = require('cors');
-const ExcelJS = require('excel');
+const ExcelJS = require('exceljs');
 const path = require('path');
 const fs = require('fs');
 
